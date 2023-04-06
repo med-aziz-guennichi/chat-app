c72da39ac98eaeca5cbd991a89268b5dc955cc4c
azizguennichi <ezzoguennichi@gmail.com>
2023-04-06 22:59:08 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import {Route} from 'react-router-dom'
+import ChatPage from './Pages/ChatPage';
+import Homepage from './Pages/Homepage';
+function App() {
+  
+  return (
+    <div className="App">
+      <Route path="/" component={Homepage} exact/>
+      <Route path="/chats" component={ChatPage} exact />
+    </div>
+  );
+}
+
+export default App;
